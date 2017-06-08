8ce276ce2b5cc5208b999054ae1e8d0d8c447d5c
PMARINA <bluerat33@gmail.com>
2017-06-08 13:25:33 -0400

    uploading to work on sch wifi

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -28,7 +28,7 @@ public class SeamCarver {
 			//do normal thing
 			Color pic = naPhoto.get(x, y);
 			Color picxp1 = naPhoto.get(x+1, y);
-			Color picyp1 = naPhoto.get(x, x+1);
+			Color picyp1 = naPhoto.get(x, y+1);
 			Color picxm1 = naPhoto.get(x-1, y);
 			Color picym1 = naPhoto.get(x, y-1);
 			double xgradred = Math.abs(picxp1.getRed() - picxm1.getRed());
@@ -48,7 +48,72 @@ public class SeamCarver {
 			//x = border = bottom
 			// y = border = right
 			if(x == 0){
-				
+				Color pic = naPhoto.get(x, y);
+				Color picxp1 = naPhoto.get(x+1, y);
+				Color picyp1 = naPhoto.get(x, y+1);
+				Color picxm1 = naPhoto.get(height()-1, y);
+				Color picym1 = naPhoto.get(x, y-1);
+				double xgradred = Math.abs(picxp1.getRed() - picxm1.getRed());
+				double ygradred = Math.abs(picxp1.getRed() - picym1.getRed());
+				double xgradblue = Math.abs(picxp1.getBlue()-picxm1.getBlue());
+				double ygradblue = Math.abs(picyp1.getBlue()-picym1.getBlue());
+				double xgradgreen = Math.abs(picxp1.getGreen()-picxm1.getGreen());
+				double ygradgreen = Math.abs(picyp1.getGreen()-picym1.getGreen());
+				double xgrad = Math.pow(xgradred, 2) + Math.pow(xgradblue, 2) + Math.pow(xgradgreen,2);
+				double ygrad = Math.pow(ygradred, 2) + Math.pow(ygradblue, 2) + Math.pow(ygradgreen, 2);
+				double energy = xgrad + ygrad;
+				return energy;
+			}
+			else if(x == height()-1){
+				Color pic = naPhoto.get(x, y);
+				Color picxp1 = naPhoto.get(0, y);
+				Color picyp1 = naPhoto.get(x, y+1);
+				Color picxm1 = naPhoto.get(x-1, y);
+				Color picym1 = naPhoto.get(x, y-1);
+				double xgradred = Math.abs(picxp1.getRed() - picxm1.getRed());
+				double ygradred = Math.abs(picxp1.getRed() - picym1.getRed());
+				double xgradblue = Math.abs(picxp1.getBlue()-picxm1.getBlue());
+				double ygradblue = Math.abs(picyp1.getBlue()-picym1.getBlue());
+				double xgradgreen = Math.abs(picxp1.getGreen()-picxm1.getGreen());
+				double ygradgreen = Math.abs(picyp1.getGreen()-picym1.getGreen());
+				double xgrad = Math.pow(xgradred, 2) + Math.pow(xgradblue, 2) + Math.pow(xgradgreen,2);
+				double ygrad = Math.pow(ygradred, 2) + Math.pow(ygradblue, 2) + Math.pow(ygradgreen, 2);
+				double energy = xgrad + ygrad;
+				return energy;
+			}
+			else if(y == width()-1){
+				Color pic = naPhoto.get(x, y);
+				Color picxp1 = naPhoto.get(x+1, y);
+				Color picyp1 = naPhoto.get(x, 0);
+				Color picxm1 = naPhoto.get(x-1, y);
+				Color picym1 = naPhoto.get(x, y-1);
+				double xgradred = Math.abs(picxp1.getRed() - picxm1.getRed());
+				double ygradred = Math.abs(picxp1.getRed() - picym1.getRed());
+				double xgradblue = Math.abs(picxp1.getBlue()-picxm1.getBlue());
+				double ygradblue = Math.abs(picyp1.getBlue()-picym1.getBlue());
+				double xgradgreen = Math.abs(picxp1.getGreen()-picxm1.getGreen());
+				double ygradgreen = Math.abs(picyp1.getGreen()-picym1.getGreen());
+				double xgrad = Math.pow(xgradred, 2) + Math.pow(xgradblue, 2) + Math.pow(xgradgreen,2);
+				double ygrad = Math.pow(ygradred, 2) + Math.pow(ygradblue, 2) + Math.pow(ygradgreen, 2);
+				double energy = xgrad + ygrad;
+				return energy;
+			}
+			else{
+				Color pic = naPhoto.get(x, y);
+				Color picxp1 = naPhoto.get(x+1, y);
+				Color picyp1 = naPhoto.get(x, y+1);
+				Color picxm1 = naPhoto.get(x-1, y);
+				Color picym1 = naPhoto.get(x, height()-1);
+				double xgradred = Math.abs(picxp1.getRed() - picxm1.getRed());
+				double ygradred = Math.abs(picxp1.getRed() - picym1.getRed());
+				double xgradblue = Math.abs(picxp1.getBlue()-picxm1.getBlue());
+				double ygradblue = Math.abs(picyp1.getBlue()-picym1.getBlue());
+				double xgradgreen = Math.abs(picxp1.getGreen()-picxm1.getGreen());
+				double ygradgreen = Math.abs(picyp1.getGreen()-picym1.getGreen());
+				double xgrad = Math.pow(xgradred, 2) + Math.pow(xgradblue, 2) + Math.pow(xgradgreen,2);
+				double ygrad = Math.pow(ygradred, 2) + Math.pow(ygradblue, 2) + Math.pow(ygradgreen, 2);
+				double energy = xgrad + ygrad;
+				return energy;
 			}
 		}
 	}
